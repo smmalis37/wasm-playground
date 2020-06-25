3dd22f4a1b4d5f2352a0f78d64015a2d0ea671d8
Steven Malis <smmalis37@gmail.com>
2020-06-25 15:27:02 -0700

    First game of life.

---
+++ README.md
@@ -0,0 +1,14 @@
+# wasm-playground
+
+Just messing around with Rust and wasm. Code based on https://rustwasm.github.io/docs/book/game-of-life.
+
+### Build 
+```
+wasm-pack build
+```
+
+### Run locally
+```
+cd www
+npm run start
+```
---
+++ www/index.js
@@ -0,0 +1,80 @@
+import { Universe, Cell } from "wasm-playground";
+import { memory } from "wasm-playground/wasm_playground_bg"
+
+const width = 64;
+const height = 64;
+const CELL_SIZE = 8; // px
+
+const canvas = document.getElementById("game-of-life-canvas");
+const universe = Universe.new(width, height);
+const ctx = canvas.getContext('2d');
+
+const GRID_COLOR = "#CCCCCC";
+const DEAD_COLOR = "#FFFFFF";
+const ALIVE_COLOR = "#000000";
+
+canvas.height = (CELL_SIZE + 1) * height + 1;
+canvas.width = (CELL_SIZE + 1) * width + 1;
+
+const getIndex = (row, column) => {
+    return row * width + column;
+};
+
+const drawGrid = () => {
+    ctx.beginPath();
+    ctx.strokeStyle = GRID_COLOR;
+
+    // Vertical lines.
+    for (let i = 0; i <= width; i++) {
+        ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+        ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+    }
+
+    // Horizontal lines.
+    for (let j = 0; j <= height; j++) {
+        ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
+        ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
+    }
+
+    ctx.stroke();
+};
+
+const drawCells = () => {
+    const cellsPtr = universe.cells();
+    const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+
+    ctx.beginPath();
+
+    for (let row = 0; row < height; row++) {
+        for (let col = 0; col < width; col++) {
+            const idx = getIndex(row, col);
+
+            ctx.fillStyle = cells[idx] === Cell.Dead
+                ? DEAD_COLOR
+                : ALIVE_COLOR;
+
+            ctx.fillRect(
+                col * (CELL_SIZE + 1) + 1,
+                row * (CELL_SIZE + 1) + 1,
+                CELL_SIZE,
+                CELL_SIZE
+            );
+        }
+    }
+
+    ctx.stroke();
+};
+
+const updateUI = () => {
+    drawGrid();
+    drawCells();
+}
+
+const renderLoop = () => {
+    universe.tick();
+    updateUI();
+    requestAnimationFrame(renderLoop);
+};
+
+updateUI();
+requestAnimationFrame(renderLoop);
