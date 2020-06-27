a7b22869c2110aa437c35ca12c39f7d8881d27bf
smmalis37 <smmalis37@users.noreply.github.com>
2020-06-27 03:31:28 +0000

    deploy: a6162a00ed32be60e3d478728531f6ee4daf7e1c

--- bootstrap.js
+++ bootstrap.js
@@ -183,7 +183,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/wasm_playground_bg.wasm":"601abbf3ae5ebd463e25"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/wasm_playground_bg.wasm":"0250a2317f965fde4960"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
